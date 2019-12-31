18169babccc9bd3892cbb25825e40738551a3eae
Lapys <oluwafunmitoo@gmail.com>
2019-12-31 01:43:45 +0100

    Update resource

--- Technologies/C/Function Overloading/function-overloading.c
+++
@@ -1,9 +0,0 @@
-/* Definition > (Overload Function, ...) */
-#define overloadFunction(function_name, function_return_types, function_parameters, function_source_codes) \
-    void function_name() {}
-
-/* Main */
-int main(void) {
-    // Return
-    return 0;
-}
