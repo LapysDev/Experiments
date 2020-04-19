bd5e34ac0347da0722045a5d75cb1ad66f53bc6b
Lapys <oluwafunmitoo@gmail.com>
2020-04-19 03:18:11 +0100

    Update resource

--- Technologies/C++/Numbers/bit-manipulation.cpp
+++
@@ -1,8 +0,0 @@
-/* Function > ... */
-constexpr inline int clear_bit(int const number, size_t const index) noexcept { return number & ~(1uLL << index); }
-constexpr inline int get_bit(int const number, size_t const index) noexcept { return ((number >> index) & 1uLL) ? 1 : 0; }
-constexpr inline int set_bit(int const number, size_t const index) noexcept { return number | (1uLL << index); }
-constexpr inline int toggle_bit(int const number, size_t const index) noexcept { return number ^ (1uLL << index); }
-
-/* Main */
-int main(void) { return 0; }
