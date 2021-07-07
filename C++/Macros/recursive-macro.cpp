6a39f051de2f1ed97e06692ba3c8c57480b2f750
LapysDev <lapysdev@gmail.com>
2021-07-07 02:50:55 +0100

    Update resource

--- C++/Macros/recursive-macro.cpp
+++ C++/Macros/recursive-macro.cpp
@@ -1,50 +1,52 @@
 #include <cstdio>
 
 /* ... */
-#define boolean(argument) is_probe(concatenate(boolean_select_, argument))
-#define boolean_select_0 ~, 0
+#define concatenate(argument1, argument2) argument1 ## argument2
+#define defer(macro, ...) macro(__VA_ARGS__)
+#define first(argument, ...) argument
+#define second(argument1, argument2, ...) argument2
 
-#define check(argument) check_select(boolean(argument))
-#define check_select(argument) concatenate(check_select_, argument)
-#define check_select_0(...)
-#define check_select_1(...) __VA_ARGS__
-
-#define concatenate(a, b) a ## b
-
-#define defer1(x) x empty()
-#define defer2(x) x empty empty()()
-#define defer3(x) x empty empty empty()()()
-#define defer4(x) x empty empty empty empty()()()()
-
-#define empty()
-
-#define first(a, ...) a
-#define second(a, b, ...) b
-
-#define evaluate(...)     evaluate1024(__VA_ARGS__)
-#define evaluate1024(...) evaluate512(evaluate512(__VA_ARGS__))
-#define evaluate512(...)  evaluate256(evaluate256(__VA_ARGS__))
-#define evaluate256(...)  evaluate128(evaluate128(__VA_ARGS__))
-#define evaluate128(...)  evaluate64(evaluate64(__VA_ARGS__))
-#define evaluate64(...)   evaluate32(evaluate32(__VA_ARGS__))
-#define evaluate32(...)   evaluate16(evaluate16(__VA_ARGS__))
-#define evaluate16(...)   evaluate8(evaluate8(__VA_ARGS__))
-#define evaluate8(...)    evaluate4(evaluate4(__VA_ARGS__))
-#define evaluate4(...)    evaluate2(evaluate2(__VA_ARGS__))
-#define evaluate2(...)    evaluate1(evaluate1(__VA_ARGS__))
-#define evaluate1(...)    __VA_ARGS__
-
-#define is_probe(...) second(__VA_ARGS__, 1)
-
-#define has_arguments(...) boolean(first(argument_end __VA_ARGS__)())
-#define argument_end() 0
-
-#define map(function, argument, ...) function(argument) check(has_arguments(__VA_ARGS__)) (defer2(map_recurse)()(function, __VA_ARGS__))
-#define map_recurse() map
+#define map(...) map_parse(map_begin(__VA_ARGS__))
+# define map_arity_pass(...) __VA_ARGS__
+# define map_arity_fail(...)
+# define map_check(macro, argument) defer(concatenate, macro ## _, defer(second, concatenate(map_check_, argument), pass))
+#   define map_check_fail ~, fail
+#   define map_check_pass
+# define map_begin(function, separator, argument, ...) function(argument) map_check(map_separator, first(map_niladic_check __VA_ARGS__)())(separator) map_check(map_arity, first(map_niladic_check __VA_ARGS__)())(map_stall(map_recurse)()(function, separator, __VA_ARGS__))
+# define map_niladic_check() fail
+# define map_parse(...) map_parse_8(__VA_ARGS__)
+#   define map_parse_1(...) __VA_ARGS__
+#   define map_parse_2(...) map_parse_1(map_parse_1(__VA_ARGS__))
+#   define map_parse_3(...) map_parse_2(map_parse_2(__VA_ARGS__))
+#   define map_parse_4(...) map_parse_3(map_parse_3(__VA_ARGS__))
+#   define map_parse_5(...) map_parse_4(map_parse_4(__VA_ARGS__))
+#   define map_parse_6(...) map_parse_5(map_parse_5(__VA_ARGS__))
+#   define map_parse_7(...) map_parse_6(map_parse_6(__VA_ARGS__))
+#   define map_parse_8(...) map_parse_7(map_parse_7(__VA_ARGS__))
+# define map_separator_pass(separator) separator()
+# define map_separator_fail(separator)
+# define map_stall(argument) map_stall_8(argument)
+# define map_stall_null()
+#   define map_stall_1(argument) argument map_stall_null()
+#   define map_stall_2(argument) argument map_stall_null map_stall_null()()
+#   define map_stall_3(argument) argument map_stall_null map_stall_null map_stall_null()()()
+#   define map_stall_4(argument) argument map_stall_null map_stall_null map_stall_null map_stall_null()()()()
+#   define map_stall_5(argument) argument map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null()()()()()
+#   define map_stall_6(argument) argument map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null()()()()()()
+#   define map_stall_7(argument) argument map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null()()()()()()()
+#   define map_stall_8(argument) argument map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null map_stall_null()()()()()()()()
+# define map_recurse() map_begin
 
 /* Main */
 #define greet(name) "Hello, \"" #name "\"!" "\r\n"
+# define space()
+#define square(literal) (literal * literal)
+# define plus() +
+#define stringify(argument) #argument
+# define comma() ,
 
 int main(void) {
-    std::printf("%s", evaluate(map(greet, C++, GD:Script, Visual Basic)));
+    std::printf("%s, %s", map(stringify, comma, Hello, number:));
+    std::printf(" %i" "\r\n\n", map(square, plus, 1, 2, 3, 4));
+    std::printf("%s", map(greet, space, C++, GD:Script, Visual Basic));
 }
