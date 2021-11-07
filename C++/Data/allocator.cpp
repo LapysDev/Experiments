4d45d0b35b4c7e2d70f5338b4d9d55adf3dd9ea0
LapysDev <oluwafunmitoo@gmail.com>
2021-11-07 06:01:27 +0100

    Write custom allocator implementation

--- C++/Data/allocator.cpp
+++ C++/Data/allocator.cpp
@@ -1,117 +1,171 @@
 #include <cstddef>
 #include <cstdio>
-#include <cstdlib>
 #include <new>
-#include <stdbool.h>
 
 /* ... */
-unsigned char BUFFER[32/*32 * 1024*/] = {};
+std::byte BUFFER[32u * 1024u] = {};
 
-void debug() {
-  unsigned char *iterator = BUFFER;
-
-  for (unsigned char iterator = 0u; iterator != 50u; ++iterator)
-  std::printf("%u ", BUFFER[iterator]);
-
-  std::putchar('{');
-  while (true) {
-    std::size_t const capacity = *std::launder(reinterpret_cast<std::size_t*>(iterator));
-    bool const reserved = *std::launder(reinterpret_cast<bool*>(iterator + sizeof(std::size_t)));
-
-    if (0u == capacity) break;
-
-    if (reserved) std::printf("%lu,  ", static_cast<unsigned long>(capacity));
-    else std::printf("%lu*, ", static_cast<unsigned long>(capacity));
-
-    iterator += capacity + sizeof(bool) + sizeof(std::size_t);
-  }
-  std::puts("0}");
-}
-
-// ...
-void* allocate(std::size_t const size) noexcept {
-  unsigned char *allocation = BUFFER;
-  std::size_t capacity;
-  std::size_t leftover = 0u;
-  bool reserved;
-  bool reused = false;
-
-  // Overflow protection --- UPDATE (Lapys) -> This doesn't actually protect
-  if (0u == size || allocation + size + sizeof(bool) + sizeof(std::size_t) > BUFFER + (sizeof(BUFFER) / sizeof(*BUFFER)))
-  return NULL;
+// ... ->> `0 != alignment + size`
+void* allocate(std::size_t const size, std::size_t const alignment = alignof(std::max_align_t)) noexcept {
+  typedef std::size_t header_t;
+  std::byte *allocation = BUFFER;
 
   // Search for unreserved region
   while (true) {
-    capacity = *std::launder(reinterpret_cast<std::size_t*>(allocation));
-    reserved = *std::launder(reinterpret_cast<bool*>(allocation + sizeof(std::size_t)));
+    header_t *const header = std::launder(reinterpret_cast<header_t*>(allocation));
+    std::size_t capacity = *reinterpret_cast<std::size_t*>(header) >> 1u;
+    bool const reserved = *reinterpret_cast<std::size_t*>(header) & 1u;
 
-    // Found untouched part of `BUFFER`
-    if (0u == capacity)
-    break;
+    // Found un-committed region
+    if (0u == capacity) {
+      std::byte *buffer;
 
-    // Found recently unreserved region
+      allocation += sizeof(header_t);
+      capacity = size;
+
+      while (0u != (allocation - BUFFER) % alignment) {
+        ++allocation;
+        ++capacity;
+      }
+
+      // Overflow protection
+      if (allocation + size >= BUFFER + (sizeof(BUFFER) / sizeof(*BUFFER)))
+      return NULL;
+
+      // Mark header for next region
+      buffer = allocation + size;
+      while (0u != (buffer - BUFFER) % alignof(header_t)) ++buffer;
+
+      new (buffer) header_t(false | (0u << 1u));
+
+      // Commit region
+      *reinterpret_cast<std::size_t*>(header) = true | (capacity << 1u);
+      break;
+    }
+
+    // Found unreserved region
     if (false == reserved) {
-      std::size_t available = capacity;
-      unsigned char *next = allocation;
-      std::size_t nextCapacity = capacity;
-      bool nextReserved = reserved;
+      std::byte *boundary = BUFFER + (sizeof(BUFFER) / sizeof(*BUFFER));
+      std::byte *buffer = allocation;
 
+      // Merge contiguous unreserved regions
       while (true) {
-        if (0u == nextCapacity) break;
-        if (nextReserved) { available = 0u; break; }
+        header_t const *const bufferHeader = std::launder(reinterpret_cast<header_t*>(buffer));
+        std::size_t const bufferCapacity = *reinterpret_cast<std::size_t const*>(bufferHeader) >> 1u;
+        bool const bufferReserved = *reinterpret_cast<std::size_t const*>(bufferHeader) & 1u;
 
-        if (available > size + sizeof(bool) + sizeof(std::size_t)) {
-          leftover = available - size;
+        // De-commit regions (found untouched region)
+        if (0u == bufferCapacity) {
+          capacity = 0u;
           break;
         }
 
-        next += nextCapacity + sizeof(bool) + sizeof(std::size_t);
-        nextCapacity = *std::launder(reinterpret_cast<std::size_t*>(next));
-        nextReserved = *std::launder(reinterpret_cast<bool*>(next + sizeof(std::size_t)));
-        available += nextCapacity;
+        // Merge regions
+        if (false != bufferReserved) {
+          boundary = buffer;
+          capacity = (buffer - allocation) - sizeof(header_t);
+          break;
+        }
+
+        // ... next region
+        buffer += bufferCapacity + sizeof(header_t);
+        while (0u != (buffer - BUFFER) % alignof(header_t)) ++buffer;
+
+        // De-commit regions (found the end)
+        if (buffer >= (BUFFER + (sizeof(BUFFER) / sizeof(*BUFFER))) - sizeof(header_t)) {
+          capacity = 0u;
+          break;
+        }
       }
 
-      if (0u != available) {
-        reused = true;
+      *reinterpret_cast<std::size_t*>(header) = false | (capacity << 1u);
+
+      // ...
+      if (0u == capacity)
+      continue;
+
+      if (capacity >= size) {
+        std::byte *body = allocation + sizeof(header_t);
+
+        // ...
+        while (0u != (body - BUFFER) % alignment) ++body;
+
+        buffer = body + size;
+        while (0u != (buffer - BUFFER) % alignof(header_t)) ++buffer;
+
+        // Possibly mark header for next region
+        if (boundary <= buffer || static_cast<std::size_t>(boundary - buffer) <= sizeof(header_t)) buffer = boundary;
+        else new (buffer) header_t(false | (((boundary - buffer) - sizeof(header_t)) << 1u));
+
+        // Commit region
+        *reinterpret_cast<std::size_t*>(header) = true | (((buffer - allocation) - sizeof(header_t)) << 1u);
+        allocation = body;
+
         break;
       }
     }
 
-    // Next region
-    allocation += capacity + sizeof(bool) + sizeof(std::size_t);
-  }
+    // ... next region
+    allocation += capacity + sizeof(header_t);
+    while (0u != (allocation - BUFFER) % alignof(header_t)) ++allocation;
 
-  // Imprint metadata in allocated region
-  new (allocation) std::size_t(size);
-  new (allocation + sizeof(std::size_t)) bool(true);
-
-  // Instantiate metadata for next region
-  if (false == (0u == leftover && reused)) {
-    new (allocation + size + sizeof(bool) + sizeof(std::size_t)) std::size_t(leftover);
-    new (allocation + size + sizeof(bool) + sizeof(std::size_t) + sizeof(std::size_t)) bool(false);
+    if (allocation >= (BUFFER + (sizeof(BUFFER) / sizeof(*BUFFER))) - sizeof(header_t))
+    return NULL;
   }
 
   // ...
-  return allocation + sizeof(bool) + sizeof(std::size_t);
+  return allocation;
 }
 
-void reallocate(void* const, std::size_t const) noexcept {}
-
+// ... ->> `NULL != pointer && pointer in BUFFER`
 void release(void* const pointer) noexcept {
-  *std::launder(reinterpret_cast<bool*>(static_cast<unsigned char*>(pointer) - sizeof(bool))) = false;
+  typedef std::size_t header_t;
+  std::byte *buffer = BUFFER;
+
+  for (std::byte *iterator = BUFFER; ; ) {
+    header_t *header = std::launder(reinterpret_cast<header_t*>(iterator));
+    std::size_t const capacity = *reinterpret_cast<std::size_t*>(header) >> 1u;
+
+    // Found `pointer` within `BUFFER`
+    if (iterator > pointer) {
+      header = std::launder(reinterpret_cast<header_t*>(buffer));
+      *reinterpret_cast<std::size_t*>(header) &= ~static_cast<std::size_t>(1u);
+
+      break;
+    }
+
+    // ... next region
+    buffer = iterator;
+    iterator += capacity + sizeof(header_t);
+    while (0u != (iterator - BUFFER) % alignof(header_t)) ++iterator;
+  }
 }
 
 /* Main */
 int main() {
-  new (BUFFER + 0)                   std::size_t(0u);
-  new (BUFFER + sizeof(std::size_t)) bool(false);
+  new (BUFFER) std::size_t(false | (0u << 1u));
+
+  /* ... */
+  constexpr unsigned char count = 10u;
+  void *allocations[count] = {};
 
   // ...
-  constexpr unsigned char count = 10u;
-  unsigned char *items[count] = {};
-
-  for (unsigned char iterator = 0u; count != iterator; ++iterator) {
-    items[iterator] = static_cast<unsigned char*>(allocate(13u));
-    std::printf("[]: %i" "\r\n", items[iterator] - BUFFER);
+  for (unsigned char index = 0u; count != index; ++index) {
+    allocations[index] = allocate(64u);
+    std::printf("[allocate(64) {%u}]: %lli" "\r\n", index, static_cast<std::byte*>(allocations[index]) - BUFFER);
   }
+
+  release(allocations[2]); std::puts("[release {2}]");
+  release(allocations[4]); std::puts("[release {4}]");
+  release(allocations[5]); std::puts("[release {5}]");
+  release(allocations[7]); std::puts("[release {7}]");
+
+  for (unsigned char counter = 3u; counter; --counter)
+  std::printf("[allocate(64)]: %lli" "\r\n", static_cast<std::byte*>(allocate(64u)) - BUFFER);
+
+  for (unsigned char size = 16u; size; size /= 2u)
+  for (unsigned char counter = 3u; counter; --counter)
+  std::printf("[allocate(%u)]: %lli" "\r\n", size, static_cast<std::byte*>(allocate(size)) - BUFFER);
+
+  std::printf("[allocate(1)]: %lli", static_cast<std::byte*>(allocate(1u)) - BUFFER);
 }
